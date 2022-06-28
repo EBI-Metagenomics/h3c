df6acfca6a3f24e21f69e27195da92f83c33bdc4
Danilo Horta <danilo.horta@pm.me>
2022-06-28 14:58:53 +0100

    Testing now while reallocing results

--- test/h3client.c
+++ test/h3client.c
@@ -16,6 +16,8 @@
 #include <string.h>
 #include <sys/stat.h>
 
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
 static char const *h3master_address(void)
 {
     char const *addr = getenv("H3CLIENT_MASTER_ADDRESS");
@@ -35,22 +37,22 @@ static char const *h3master_address(void)
 
 #define FAIL(msg) XFAIL(msg, __FILE__, __LINE__)
 
-static struct h3c_result *result = 0;
 static int exit_status = 0;
 static char const cmd[] = "--hmmdb 1 --acc --cut_ga";
 
 static int test_open_close_connection(uint16_t ross_id)
 {
-    if (h3c_open(h3master_address(), 51370 + ross_id)) FAIL("h3c_open");
+    if (h3c_open(h3master_address(), 51370 + ross_id + 1)) FAIL("h3c_open");
     if (h3c_close()) FAIL("h3c_close");
 
 cleanup:
     return exit_status;
 }
 
-static int create_result_ross(uint16_t ross_id)
+static struct h3c_result *create_result_ross(uint16_t ross_id)
 {
-    if (h3c_open(h3master_address(), 51370 + ross_id)) FAIL("h3c_open");
+    struct h3c_result *result = 0;
+    if (h3c_open(h3master_address(), 51371 + ross_id)) FAIL("h3c_open");
 
     FILE *file = 0;
     if (!(file = fopen(ASSETS "/ross.fasta", "r"))) FAIL("fopen");
@@ -61,7 +63,15 @@ static int create_result_ross(uint16_t ross_id)
     if (h3c_close()) FAIL("h3c_close");
 
 cleanup:
-    return exit_status;
+    if (exit_status)
+    {
+        if (result)
+        {
+            h3c_result_del(result);
+            result = 0;
+        }
+    }
+    return result;
 }
 
 static int check_hash(char const *filepath, int64_t hash, char const *source,
@@ -83,7 +93,8 @@ cleanup:
 
 static int test_pack_result(uint16_t ross_id)
 {
-    if (create_result_ross(ross_id)) goto cleanup;
+    struct h3c_result *result = create_result_ross(ross_id);
+    if (!result) goto cleanup;
 
     FILE *file = 0;
     if (!(file = fopen(TMPDIR "/h3result.mp", "wb"))) FAIL("fopen");
@@ -93,13 +104,14 @@ static int test_pack_result(uint16_t ross_id)
     if (CHECK_HASH(TMPDIR "/h3result.mp", -7580715214164890760LL)) goto cleanup;
 
 cleanup:
-    h3c_result_del(result);
+    if (result) h3c_result_del(result);
     return exit_status;
 }
 
 static int test_print_targets(uint16_t ross_id)
 {
-    if (create_result_ross(ross_id)) goto cleanup;
+    struct h3c_result *result = create_result_ross(ross_id);
+    if (!result) goto cleanup;
 
     FILE *file = 0;
     if (!(file = fopen(TMPDIR "/targets.txt", "wb"))) FAIL("fopen");
@@ -109,13 +121,14 @@ static int test_print_targets(uint16_t ross_id)
     if (CHECK_HASH(TMPDIR "/targets.txt", 2235430570033520642LL)) goto cleanup;
 
 cleanup:
-    h3c_result_del(result);
+    if (result) h3c_result_del(result);
     return exit_status;
 }
 
 static int test_print_domains(uint16_t ross_id)
 {
-    if (create_result_ross(ross_id)) goto cleanup;
+    struct h3c_result *result = create_result_ross(ross_id);
+    if (!result) goto cleanup;
 
     FILE *file = 0;
     if (!(file = fopen(TMPDIR "/domains.txt", "wb"))) FAIL("fopen");
@@ -125,16 +138,78 @@ static int test_print_domains(uint16_t ross_id)
     if (CHECK_HASH(TMPDIR "/domains.txt", 450185627565076573LL)) goto cleanup;
 
 cleanup:
-    h3c_result_del(result);
+    if (result) h3c_result_del(result);
+    return exit_status;
+}
+
+static int test_reuse_results(void)
+{
+    struct h3c_result *result = 0;
+    FILE *file = 0;
+
+    if (!(result = h3c_result_new())) FAIL("h3c_result_new")
+
+    for (unsigned i = 0; i < 5; ++i)
+    {
+        if (!(file = fopen(ASSETS "/ross.fasta", "r"))) FAIL("fopen");
+        if (h3c_open(h3master_address(), 51371 + i)) FAIL("h3c_open");
+        if (h3c_call(cmd, file, result)) FAIL("h3c_call");
+        if (h3c_close()) FAIL("h3c_close");
+        fclose(file);
+    }
+
+cleanup:
+    if (result) h3c_result_del(result);
+    return exit_status;
+}
+
+static int test_reuse_results_print_targets(void)
+{
+    int64_t targets[] = {3595942196364536930LL, -2350707101380469820LL,
+                         7268596939732165182LL, 624009942923406464LL,
+                         2235430570033520642LL};
+    int64_t domains[] = {3742250844459566216LL, 6798122216300339939LL,
+                         -6277080907676284547LL, -5934588947600765520LL,
+                         450185627565076573LL};
+    struct h3c_result *result = 0;
+    FILE *file = 0;
+
+    if (!(result = h3c_result_new())) FAIL("h3c_result_new")
+
+    for (unsigned i = 0; i < 5; ++i)
+    {
+        if (!(file = fopen(ASSETS "/ross.fasta", "r"))) FAIL("fopen");
+        if (h3c_open(h3master_address(), 51371 + i)) FAIL("h3c_open");
+        if (h3c_call(cmd, file, result)) FAIL("h3c_call");
+        if (h3c_close()) FAIL("h3c_close");
+        fclose(file);
+
+        file = 0;
+        if (!(file = fopen(TMPDIR "/targets.txt", "wb"))) FAIL("fopen");
+        h3c_result_print_targets(result, file);
+        fclose(file);
+        if (CHECK_HASH(TMPDIR "/targets.txt", targets[i])) goto cleanup;
+
+        file = 0;
+        if (!(file = fopen(TMPDIR "/domains.txt", "wb"))) FAIL("fopen");
+        h3c_result_print_domains(result, file);
+        fclose(file);
+        if (CHECK_HASH(TMPDIR "/domains.txt", domains[i])) goto cleanup;
+    }
+
+cleanup:
+    if (result) h3c_result_del(result);
     return exit_status;
 }
 
 int main(void)
 {
-    if (test_open_close_connection(5)) goto cleanup;
-    if (test_pack_result(5)) goto cleanup;
-    if (test_print_targets(5)) goto cleanup;
-    if (test_print_domains(5)) goto cleanup;
+    if (test_open_close_connection(4)) goto cleanup;
+    if (test_pack_result(4)) goto cleanup;
+    if (test_print_targets(4)) goto cleanup;
+    if (test_print_domains(4)) goto cleanup;
+    if (test_reuse_results()) goto cleanup;
+    if (test_reuse_results_print_targets()) goto cleanup;
 
 cleanup:
     return exit_status;
