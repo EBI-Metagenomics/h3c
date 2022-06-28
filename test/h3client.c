09e0253b47f47cacb4679f63eec05a8d538df15a
Danilo Horta <danilo.horta@pm.me>
2022-06-28 10:26:33 +0100

    Organize testing

--- test/h3client.c
+++ test/h3client.c
@@ -22,72 +22,119 @@ static char const *h3master_address(void)
     return addr ? addr : "127.0.0.1";
 }
 
-static struct h3c_result *result = 0;
-
-#define FAIL(msg)                                                              \
+#define XFAIL(msg, file, line)                                                 \
     do                                                                         \
     {                                                                          \
         exit_status = 1;                                                       \
+        printf("%s:%d: ", file, line);                                         \
+        printf("failed to ");                                                  \
         printf(msg);                                                           \
         putchar('\n');                                                         \
         goto cleanup;                                                          \
     } while (0);
 
+#define FAIL(msg) XFAIL(msg, __FILE__, __LINE__)
+
+static struct h3c_result *result = 0;
+static int exit_status = 0;
+static char const cmd[] = "--hmmdb 1 --acc --cut_ga";
+
+static int test_open_close_connection(void)
+{
+    if (h3c_open(h3master_address(), 51371)) FAIL("h3c_open");
+    if (h3c_close()) FAIL("h3c_close");
+
+cleanup:
+    return exit_status;
+}
+
+static int create_result_ross(void)
+{
+    if (h3c_open(h3master_address(), 51371)) FAIL("h3c_open");
+
+    FILE *file = 0;
+    if (!(file = fopen(ASSETS "/ross.fasta", "r"))) FAIL("fopen");
+    if (!(result = h3c_result_new())) FAIL("h3c_result_new")
+    if (h3c_call(cmd, file, result)) FAIL("h3c_call");
+    fclose(file);
+
+    if (h3c_close()) FAIL("h3c_close");
+
+cleanup:
+    return exit_status;
+}
+
+static int check_hash(char const *filepath, int64_t hash, char const *source,
+                      int line)
+{
+    FILE *file = 0;
+    int64_t h = 0;
+
+    if (!(file = fopen(filepath, "rb"))) XFAIL("fopen", source, line);
+    if (!file_hash(file, &h)) XFAIL("file_hash", source, line);
+    if (h != hash) XFAIL("match file hash", source, line);
+    fclose(file);
+
+cleanup:
+    return exit_status;
+}
+
+#define CHECK_HASH(F, H) check_hash(F, H, __FILE__, __LINE__)
+
+static int test_pack_result(void)
+{
+    if (create_result_ross()) goto cleanup;
+
+    FILE *file = 0;
+    if (!(file = fopen(TMPDIR "/h3result.mp", "wb"))) FAIL("fopen");
+    if (h3c_result_pack(result, file)) FAIL("h3c_result_pack");
+    fclose(file);
+
+    if (CHECK_HASH(TMPDIR "/h3result.mp", -7580715214164890760LL)) goto cleanup;
+
+cleanup:
+    h3c_result_del(result);
+    return exit_status;
+}
+
+static int test_print_targets(void)
+{
+    if (create_result_ross()) goto cleanup;
+
+    FILE *file = 0;
+    if (!(file = fopen(TMPDIR "/targets.txt", "wb"))) FAIL("fopen");
+    h3c_result_print_targets(result, file);
+    fclose(file);
+
+    if (CHECK_HASH(TMPDIR "/targets.txt", 2235430570033520642LL)) goto cleanup;
+
+cleanup:
+    h3c_result_del(result);
+    return exit_status;
+}
+
+static int test_print_domains(void)
+{
+    if (create_result_ross()) goto cleanup;
+
+    FILE *file = 0;
+    if (!(file = fopen(TMPDIR "/domains.txt", "wb"))) FAIL("fopen");
+    h3c_result_print_domains(result, file);
+    fclose(file);
+
+    if (CHECK_HASH(TMPDIR "/domains.txt", 450185627565076573LL)) goto cleanup;
+
+cleanup:
+    h3c_result_del(result);
+    return exit_status;
+}
+
 int main(void)
 {
-    int exit_status = 0;
-    FILE *fasta_file = 0;
-    FILE *result_file = 0;
-    FILE *targets_file = 0;
-    FILE *domains_file = 0;
-
-    if (h3c_open(h3master_address(), 51371)) FAIL("Failed to h3c_open!");
-
-    fasta_file = fopen(ASSETS "/ross.fasta", "r");
-    if (!fasta_file) FAIL("Failed to fopen!");
-
-    if (!(result = h3c_result_new())) FAIL("Failed to h3c_result_new!")
-
-    if (h3c_call("--hmmdb 1 --acc --cut_ga", fasta_file, result))
-        FAIL("Failed to h3c_call!");
-
-    fclose(fasta_file);
-
-    result_file = fopen(TMPDIR "/h3result.msgpack", "wb");
-    if (!result_file) FAIL("Failed to fopen!");
-    if (h3c_result_pack(result, result_file))
-        FAIL("Failed to h3c_result_pack!");
-    fclose(result_file);
-
-    result_file = fopen(TMPDIR "/h3result.msgpack", "rb");
-    if (!result_file) FAIL("Failed to fopen!");
-    int64_t hash = 0;
-    if (!file_hash(result_file, &hash)) FAIL("Failed to file_hash!");
-    if (hash != -7580715214164890760LL)
-        FAIL("Wrong file hash for h3result.msgpack!");
-    fclose(result_file);
-
-    if (h3c_close()) FAIL("Failed to h3c_close!");
-
-    targets_file = fopen(TMPDIR "/targets.txt", "wb");
-    h3c_result_print_targets(result, targets_file);
-    fclose(targets_file);
-
-    domains_file = fopen(TMPDIR "/domains.txt", "wb");
-    h3c_result_print_domains(result, domains_file);
-    fclose(domains_file);
-
-    targets_file = fopen(TMPDIR "/targets.txt", "rb");
-    if (!file_hash(targets_file, &hash)) FAIL("Failed to file_hash!");
-    if (hash != 2235430570033520642LL) FAIL("Wrong file hash for targets.txt!");
-    fclose(targets_file);
-
-    domains_file = fopen(TMPDIR "/domains.txt", "rb");
-    if (!file_hash(domains_file, &hash)) FAIL("Failed to file_hash!");
-    if (hash != 450185627565076573LL) FAIL("Wrong file hash for domains.txt!");
-    fclose(domains_file);
-
-    h3c_result_del(result);
+    if (test_open_close_connection()) goto cleanup;
+    if (test_pack_result()) goto cleanup;
+    if (test_print_targets()) goto cleanup;
+    if (test_print_domains()) goto cleanup;
 
 cleanup:
     return exit_status;
