bf178bf06c20dd3bb5560eee71b622e49083d6d1
Danilo Horta <danilo.horta@pm.me>
2022-11-20 14:07:10 +0000

    recv msg with callb

--- src/msg.h
+++ src/msg.h
@@ -9,6 +9,8 @@ struct nng_stream;
 
 struct msg *msend(struct nng_stream *, int len, struct nng_iov *);
 struct msg *mrecv(struct nng_stream *, int len, struct nng_iov *);
+struct msg *mrecv_callb(struct nng_stream *, size_t size, void *data,
+                        void (*callb)(void *), void *arg);
 int mwait(struct msg *);
 
 #endif
