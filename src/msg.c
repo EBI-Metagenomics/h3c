151f202cfe486922c9176f75d03cf7d26c18c7c7
Danilo Horta <danilo.horta@pm.me>
2022-11-20 20:37:56 +0000

    msg

--- src/msg.c
+++ src/msg.c
@@ -1,121 +1,113 @@
 #include "msg.h"
+#include "amsg.h"
+#include "answer.h"
 #include "h3c/code.h"
+#include "hmsg.h"
+#include "timeout.h"
 #include <nng/nng.h>
-#include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 
-struct msg
+struct msg *msg_new(struct nng_stream *stream)
 {
-    size_t rem;
-    int len;
-    struct nng_iov iov[8];
-    struct nng_aio *upper_aio;
-    struct nng_aio *lower_aio;
-    struct nng_stream *s;
-    void (*submit)(struct nng_stream *, struct nng_aio *);
-};
-
-static struct msg *alloc(nng_stream *s, void (*submit)(nng_stream *, nng_aio *),
-                         int len, struct nng_iov *);
-static void destroy(struct msg *x);
-static void start(struct msg *x);
-static void callback(void *arg);
-
-struct msg *msend(struct nng_stream *s, int len, struct nng_iov *iov)
-{
-    struct msg *x = alloc(s, &nng_stream_send, len, iov);
+    struct msg *x = malloc(sizeof(*x));
     if (!x) return x;
-    start(x);
-    return x;
-}
 
-struct msg *mrecv(struct nng_stream *s, int len, struct nng_iov *iov)
-{
-    struct msg *x = alloc(s, &nng_stream_recv, len, iov);
-    if (!x) return x;
-    start(x);
-    return x;
-}
+    x->stream = stream;
+    x->state = 0;
+    x->deadline = 0;
+    x->aio = NULL;
+    x->ans = NULL;
+    x->send_amsg = NULL;
+    x->recv_hmsg = NULL;
+    cco_node_init(&x->node);
 
-int mwait(struct msg *x)
-{
-    int rv;
-    if (!x) return H3C_ENOMEM;
-    nng_aio_wait(x->upper_aio);
-    rv = nng_aio_result(x->upper_aio);
-    destroy(x);
-    return rv;
-}
-
-static struct msg *alloc(nng_stream *s, void (*submit)(nng_stream *, nng_aio *),
-                         int len, struct nng_iov *iov)
-{
-    struct msg *x;
-
-    if ((x = nng_alloc(sizeof(*x))) == NULL) return NULL;
-    if (nng_aio_alloc(&x->upper_aio, NULL, NULL) != 0)
+    if (nng_aio_alloc(&x->aio, NULL, NULL))
     {
-        destroy(x);
-        return NULL;
-    }
-    if (nng_aio_alloc(&x->lower_aio, &callback, x) != 0)
-    {
-        destroy(x);
+        free(x);
         return NULL;
     }
 
-    // Upper should not take more than 30 seconds, lower not more than 5.
-    nng_aio_set_timeout(x->upper_aio, 30000);
-    nng_aio_set_timeout(x->lower_aio, 5000);
-
-    nng_aio_begin(x->upper_aio);
-
-    x->s = s;
-    x->rem = 0;
-    for (int i = 0; i < len; ++i)
-        x->rem += iov[i].iov_len;
-    x->len = len;
-    memcpy(x->iov, iov, len * sizeof(*iov));
-    x->submit = submit;
-
+    if (!(x->ans = answer_new()))
+    {
+        nng_aio_free(x->aio);
+        free(x);
+        return NULL;
+    }
     return x;
 }
 
-static void destroy(struct msg *x)
+static void callb2(void *arg);
+
+int msg_start(struct msg *x, char const *args, char const *seq, long deadline)
+{
+    struct nng_iov iov[5] = {
+        {.iov_buf = "@", .iov_len = 1},
+        {.iov_buf = (void *)args, .iov_len = strlen(args)},
+        {.iov_buf = "\n", .iov_len = 1},
+        {.iov_buf = (void *)seq, .iov_len = strlen(seq)},
+        {.iov_buf = "//", .iov_len = 2},
+    };
+    x->state = SEND;
+    x->deadline = deadline;
+
+    nng_aio_set_timeout(x->aio, timeout(deadline));
+    nng_aio_begin(x->aio);
+
+    if (!(x->send_amsg = asend(x->stream, 5, iov, &callb2, x, deadline)))
+        return H3C_ENOMEM;
+
+    astart(x->send_amsg);
+    return H3C_OK;
+}
+
+int msg_wait(struct msg *x)
+{
+    nng_aio_wait(x->aio);
+    return nng_aio_result(x->aio);
+}
+
+void msg_cancel(struct msg *x)
+{
+    if (x->aio) nng_aio_cancel(x->aio);
+}
+
+void msg_del(struct msg *x)
 {
     if (!x) return;
-    if (x->upper_aio) nng_aio_free(x->upper_aio);
-    if (x->lower_aio) nng_aio_free(x->lower_aio);
-    nng_free(x, sizeof(*x));
+    if (x->ans) answer_del(x->ans);
+    if (x->send_amsg) adel(x->send_amsg);
+    if (x->recv_hmsg) hdel(x->recv_hmsg);
+    if (x->aio) nng_aio_free(x->aio);
+    free(x);
 }
 
-static void start(struct msg *x)
-{
-    nng_aio_set_iov(x->lower_aio, x->len, x->iov);
-    x->submit(x->s, x->lower_aio);
-}
+struct answer *msg_answer(struct msg *x) { return x->ans; }
 
-static void callback(void *arg)
+static void callb2(void *arg)
 {
     struct msg *x = arg;
-    int rv;
-    size_t n;
+    int rc = 0;
 
-    rv = nng_aio_result(x->lower_aio);
-    if (rv != 0)
+    if (x->state == SEND)
     {
-        nng_aio_finish(x->upper_aio, rv);
-        return;
+        if ((rc = aresult(x->send_amsg)))
+        {
+            nng_aio_finish(x->aio, rc);
+            return;
+        }
+
+        if (!(x->recv_hmsg = hrecv(x->stream, x->ans, &callb2, x, x->deadline)))
+        {
+            nng_aio_finish(x->aio, H3C_ENOMEM);
+            return;
+        }
+
+        x->state = RECV;
+        hstart(x->recv_hmsg);
     }
-    n = nng_aio_count(x->lower_aio);
-
-    x->rem -= n;
-
-    if (x->rem == 0)
+    else if (x->state == RECV)
     {
-        nng_aio_finish(x->upper_aio, 0);
-        return;
+        nng_aio_finish(x->aio, hresult(x->recv_hmsg));
     }
-
-    start(x);
 }
--- src/msg.h
+++ src/msg.h
@@ -1,14 +1,33 @@
 #ifndef MSG_H
 #define MSG_H
 
-#include <stddef.h>
+#include "cco.h"
 
-struct msg;
-struct nng_iov;
-struct nng_stream;
+enum state
+{
+    SEND,
+    RECV,
+};
 
-struct msg *msend(struct nng_stream *, int len, struct nng_iov *);
-struct msg *mrecv(struct nng_stream *, int len, struct nng_iov *);
-int mwait(struct msg *);
+struct msg
+{
+    struct nng_stream *stream;
+    enum state state;
+    long deadline;
+    struct nng_aio *aio;
+    struct answer *ans;
+    struct amsg *send_amsg;
+    struct hmsg *recv_hmsg;
+    struct cco_node node;
+};
+
+struct h3c_result;
+
+struct msg *msg_new(struct nng_stream *);
+int msg_start(struct msg *, char const *args, char const *seq, long deadline);
+int msg_wait(struct msg *);
+void msg_cancel(struct msg *);
+void msg_del(struct msg *);
+struct answer *msg_answer(struct msg *);
 
 #endif
