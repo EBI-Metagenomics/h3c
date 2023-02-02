4836ecf8c0371f972053b9ec36a3b28f2ecbd113
Danilo Horta <danilo.horta@pm.me>
2023-02-02 21:29:53 +0000

    press file first

--- test/start_daemon.sh
+++ test/start_daemon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-pipx run --no-cache h3daemon start "$1" --force --port 51371
-pipx run h3daemon press "$1"
+pipx run --no-cache h3daemon press "$1"
+pipx run h3daemon start "$1" --force --port 51371
