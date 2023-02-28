586f6f8313390b289853896d71333844ca8472b8
Danilo Horta <danilo.horta@pm.me>
2023-02-28 11:06:30 +0000

    isready

--- test/start_daemon.sh
+++ test/start_daemon.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "Ponto 1"
 test -e "$1.h3f" || pipx run --spec hmmer hmmpress "$1"
-echo "Ponto 2"
 pipx run h3daemon start "$1" --port 51371
-echo "Ponto 3"
-pipx run h3daemon port "$1"
-echo "Ponto 4"
+pipx run h3daemon isready "$1" --wait
