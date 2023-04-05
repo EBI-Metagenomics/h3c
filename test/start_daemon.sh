a6e984ff4fb247483cd6ab333ff9b6e1cc047325
Danilo Horta <danilo.horta@pm.me>
2023-04-05 14:12:55 +0100

    Update project configuration and dependencies
    Increase project version, update dependency, and improve test scripts
    
    - Change project name to `h3c`
    - Increase project version to `0.10.6`
    - Update dependency `lip` from `0.5.0` to `0.5.1`
    - Remove `hmmpress` command check from `test/start_daemon.sh`
    - Change interpreter from `bash` to `sh` in `test/stop_daemon.sh`

--- test/start_daemon.sh
+++ test/start_daemon.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
 
-test -e "$1.h3f" || pipx run --spec hmmer hmmpress "$1"
 pipx run h3daemon start "$1" --port 51371
 pipx run h3daemon isready "$1" --wait
--- test/stop_daemon.sh
+++ test/stop_daemon.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 pipx run h3daemon stop "$1"
